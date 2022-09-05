266e895b90f4d3122c1f0d78456a31eb10fdfd5a
jolonilua <55061495+jolonilua@users.noreply.github.com>
2022-09-05 15:00:20 -0400

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4edaa5bee8msh224bc3bd44eaf97p1f77f3jsn5a08f20f9bfa',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+
+export const WEATHER_API_KEY = "9002ca0700e0658164ded71bf69cd62d";
